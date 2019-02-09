3b8d65d7cccb34dca97903ad9f6c8ba3180d6b85
Pelle van Gils <pelle@pollop.nl>
2019-02-09 21:57:37 +0100

    2_keyboard: drop key-up events

--- 2_keyboard/2_keyboard.srcs/sources_1/new/keyboardHandler.vhd
+++ 2_keyboard/2_keyboard.srcs/sources_1/new/keyboardHandler.vhd
@@ -41,10 +41,20 @@ signal  clkBuffer   : std_logic_vector(3 downto 0)  := (others => '1');
 signal  dataBuffer  : std_logic_vector(2 downto 0)  := (others => '1');
 
 -- 11-bit word as sent by the PS/2 device
-signal  dataWord    : std_logic_vector(10 downto 0)  := (others=>'0');
+signal  dataWord    : std_logic_vector(10 downto 0) := (others => '0');
 signal  wordIndex   : integer := 0;
 
+-- key-up events need to be ignored. The scancode after a key-up code (F0)
+-- will be dropped if `dropNextKey` is set
+signal  dropNextKey : boolean := false;
 
+-- bcd that signals a key-up event
+constant    KEY_UP  : std_logic_vector(3 downto 0)  := X"B";
+-- bcd: ERROR: integrity check failed
+constant    BCD_ERR : std_logic_vector(3 downto 0)  := X"E";
+
+
+-- Trancode an 8-bit scancode to a corresponding 4-bit BCD value.
 function scancodeToBCD(scancode: in std_logic_vector(7 downto 0))
     return std_logic_vector is
 begin
@@ -65,6 +75,8 @@ begin
    end case;
 end scanCodeToBCD;
 
+-- Check the integrity of a PS/2 dataframe.
+-- checks for start + stop bits and parity.
 -- TODO: parity check
 function checkDataWord(word: in std_logic_vector(dataWord'HIGH downto 0))
     return boolean is
@@ -79,6 +91,9 @@ begin
 
 READ : process(sysclk)
 
+variable newBCD         : std_logic_vector(3 downto 0);
+variable dropKey        : boolean;
+
 begin
     if rising_edge(sysclk) then
         -- push new PS/2 data into the buffers
@@ -87,6 +102,7 @@ begin
 
         keyPressed          <= '0'; -- reset key pressed state
         
+        -- get PS2 data-bit on a 'falling edge' of the PS2 clock
         if clkBuffer(clkBuffer'LOW+1) = '0' and
            clkBuffer(clkBuffer'LOW+1) /= clkBuffer(clkBuffer'LOW)
            then
@@ -98,15 +114,24 @@ begin
         if wordIndex > dataWord'HIGH then
             wordIndex       <= 0;
 
-            -- check whether the data frame is valid
+            -- if the data frame is valid; get the key data from it.
             if checkDataWord(dataWord) then
-                keyData     <= scancodeToBCD(dataWord(dataWord'HIGH-2
+                newBCD      := scancodeToBCD(dataWord(dataWord'HIGH-2
                                                       downto dataWord'LOW+1));
+
+                -- check for a key-up event. on key-release the PS/2 keyboard
+                -- sends a KEY_UP byte followed by the pressed key's scancode.
+                dropKey     := dropNextKey or newBcd = KEY_UP;
+                dropNextKey <= newBcd = KEY_UP;
+                keyData     <= newBCD;
             else
-                keyData     <= X"E"; -- error, integrity check failed
+                keyData     <= BCD_ERR; -- error, integrity check failed
             end if; -- checkDataWord
-            keyPressed      <= '1';
-            dataWord        <= (others => '0');
+            
+            if not dropKey then
+                keyPressed  <= '1';
+                dataWord    <= (others => '0');
+            end if; -- dropKey
         end if; -- wordIndex = 11
     end if; -- rising_edge(sysclk)
     
