d685898c1e391b71a9f00fc81e0db2c0ab3fd844
Pelle van Gils <pelle@pollop.nl>
2019-02-22 14:42:01 +0100

    4-microblaze: add c projects

---
+++ 4_microblaze/4_microblaze.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_4_microblaze_c.elf_on_local.tcl
@@ -0,0 +1,8 @@
+connect -url tcp:127.0.0.1:3121
+configparams mdm-detect-bscan-mask 2
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183A3875CA"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183A3875CA"} -index 0
+dow /media/sf_shared/PROGH2/4_microblaze/4_microblaze.sdk/4_microblaze_c/Debug/4_microblaze_c.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183A3875CA"} -index 0
+con
